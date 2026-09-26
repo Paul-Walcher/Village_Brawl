a482a03b96afbf1057b4c7ec1c0367c1c2a914cd
Paul-Walcher <paulwalcher12@gmail.com>
2026-09-26 02:41:09 +0200

    auto-upload

--- src/import_folder.py
+++ src/import_folder.py
@@ -1,13 +1,6 @@
 import os
-import importlib.util
+import importlib
 
-folder = "my_folder"
-
-for filename in os.listdir(folder):
-    if filename.endswith(".py"):
-        path = os.path.join(folder, filename)
-        module_name = filename[:-3]
-
-        spec = importlib.util.spec_from_file_location(module_name, path)
-        module = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(module)
+for filename in os.listdir("folder"):
+    if filename.endswith(".py") and filename != "__init__.py":
+        importlib.import_module(f"Cards.{filename[:-3]}")
